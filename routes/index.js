a809672a814f9fb3e71a4e4c26c89b5052b14aff
Chaadaam <damchaillou@hotmail.com>
2023-01-20 15:00:52 +0100

    modification

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // création de ma route.
 router.get("/year", function (req, res) {
   const year = Date.now();
-  res.json({ now: year.getFullYear() });
+  res.json({ now: year});
 });
 
 module.exports = router;
