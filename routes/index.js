ff64c59b56466c87b3448d6c8e46a1d755aa5f72
Chaadaam <damchaillou@hotmail.com>
2023-01-20 15:59:53 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // création de ma route.
 router.get("/year", function (req, res) {
   const year = new Date().getFullYear().toString();
-  res.json({ data: year});
+  res.json({ year: year});
 });
 
 module.exports = router;
